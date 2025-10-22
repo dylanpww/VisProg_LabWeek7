26a17944575ea0d6368c6be5e00e758fc5555c98
ShatryaChristiano <schristiano@student.ciputra.ac.id>
2025-10-22 09:25:56 +0700

    ini lan udah aku tambahin preview

---
+++ app/src/main/java/com/example/labweek7/wkawowk/p.kt
@@ -0,0 +1,16 @@
+package com.example.labweek7.wkawowk
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun P (){
+    Text(text = "GUOBLOOOKKKKKKKKK")
+}
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+fun PPreview(){
+    P()
+}
